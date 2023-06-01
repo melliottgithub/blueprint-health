9a6b6635e262704bd67efa7899b7a2b33ad3231f
Mike <kllmmc23@gmail.com>
2023-06-01 09:40:07 -0600

    Fix API endpoint

--- services/src/api/assessment.ts
+++ services/src/api/assessment.ts
@@ -3,16 +3,18 @@ import { Assessment } from '../assessments';
 
 const api = express.Router();
 
-api.post('/', (req, res) => {
+api.post('/', async (req, res) => {
     const answers = req.body.answers;
     try {
-        const assessment = new Assessment(answers);
+        const assessment = await Assessment.new(answers);
+        assessment.saveWithAnswers();
         const scores = assessment.score();
         res.json({
             scores,
             results: assessment.getResult()
         });
     } catch (error) {
+        console.log(error);
         const err = error as Error;
         res.status(400).json({
             error: err.message
